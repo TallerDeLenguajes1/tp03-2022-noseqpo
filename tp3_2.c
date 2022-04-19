1c157cc6b906df7586355f18ef581a849a10b011
Daniel Paz <danielnicolaspeace@gmail.com>
2022-04-19 16:06:26 -0300

    arreglo punto 2

--- tp3_2.c
+++ tp3_2.c
@@ -2,24 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+void cargarNombre(char **p)
+{
+    char buff[50];
+    printf("\nIngrese un name:");
+    gets(buff);
+    *p = (char *)malloc((strlen(buff) + 1) * sizeof(char));
+    strcpy(*p,buff);
+    // free nada?? nada, porque buff es de la funcion y si libero *p lo pierdo
+}
+
 int main(int argc, char const *argv[])
 {
-    char *array[5];
-    char temp[10]; // cual es el punto de usar malloc acá? 
+    /*char *temp; // no tiene memoria
+    cargarNombre(&temp); // tenemos que enviar la direccion de nuestro puntero
+    puts(temp); // hay que hacer de cuenta que acepta **strings** directamente
+    */
+
+    char *V[5]; // arreglo de 5 punteros tipo char
+
     for (int i = 0; i < 5; i++)
     {
-        printf("\nIngrese un nombre: ");
-        gets(temp);
-
-        *(array + i) = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
-        strcpy(*(array + i), temp);
+        cargarNombre(&V[i]);
     }
 
     for (int i = 0; i < 5; i++)
     {
-        puts(*(array+i));
-        free(array[i]);
-    }
+        puts(V[i]); 
+        free(V[i]); // uno por uno si o si
+    } // se podria mostrar y liberar en el mismo momento que se carga? posiblemente
 
+    // perfecto!!
+    
     return 0;
 }
--- tp3_3.c
+++ tp3_3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-char *TiposProductos[] = {"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
+char *TiposProductos[] = {"Galletas", "Snack", "Cigarrillos", "Caramelos", "Bebidas"};
 struct Producto
 {
     int ProductoID;       // Numerado en ciclo iterativo
@@ -13,12 +16,100 @@ struct Cliente
     int ClienteID;               // Numerado en el ciclo iterativo
     char *NombreCliente;         // Ingresado por usuario
     int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
-    Producto *Productos          // El tamaño de este arreglo depende de la variable
-    // "CantidadProductosAPedir"
+    Producto *Productos;         // El tamaño de este arreglo depende de la variable "CantidadProductosAPedir"
 } typedef Cliente;
+
+int pedirClientes();
+Cliente cargarCliente(int id);
+void cargarProductos(Producto *productos, int tam);
+int costoProducto(Producto *temp);
+void mostrarTodo(Cliente *listado, int tam);
+void imprimirProductos(Producto **listado, int tam);
 
 int main(int argc, char const *argv[])
 {
-
+    srand(time(NULL));
+    int clientes = pedirClientes();
+    Cliente listado[clientes];
+    for (int i = 0; i < clientes-1; i++)
+    {
+        listado[i] = cargarCliente(i); 
+    }
+    mostrarTodo(listado, clientes);
     return 0;
+}
+
+int pedirClientes()
+{
+    // Peventista necesita la cantidad de clientes
+    int clientes;
+    printf("\nIngrese la cantidad de clientes: ");
+    scanf("%d", &clientes);
+    return clientes;
+}
+
+Cliente cargarCliente(int id)
+{
+    Cliente temp;
+    temp.ClienteID = id+1;
+    char *bufalo = malloc(sizeof(char) * 100);
+    fflush(stdin);
+    printf("\nIngrese el nombre del cliente %d: ", id+1);
+    gets(bufalo);
+    temp.NombreCliente = malloc(sizeof(char) * (strlen(bufalo) + 1));
+    strcpy(temp.NombreCliente, bufalo);
+    free(bufalo); // proceso caro
+    // hasta acá anda bien
+    temp.CantidadProductosAPedir = 1 + rand() % 5;
+    printf("cantidad: %d", temp.CantidadProductosAPedir);
+
+    Producto productos[temp.CantidadProductosAPedir]; // hacer malloc ??
+    cargarProductos(productos, temp.CantidadProductosAPedir);
+    // imprimir productos acá
+    return temp;
+}
+
+void cargarProductos(Producto *productos, int tam)
+{
+    // acá hay problemas
+    for (int i = 0; i < tam - 1; i++)
+    {
+        Producto *temp = (productos + i);
+        temp->ProductoID = i;
+        temp->Cantidad = 1 + rand() % 10;
+        temp->TipoProducto = *(TiposProductos + (rand() % 5)); // puede acceder a los nombres???
+        temp->PrecioUnitario = 10 + rand() % 100;
+    }
+}
+
+int costoProducto(Producto *temp)
+{
+    return temp->Cantidad * temp->PrecioUnitario;
+}
+
+void mostrarTodo(Cliente *listado, int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        Cliente temp = listado[i];
+        printf("\n\n\n --------------  Cliente ID %d: ", temp.ClienteID);
+        printf("\n Nombre del Cliente: %s", temp.NombreCliente);
+        printf("\n Cantidad de productos pedidos: %d", temp.CantidadProductosAPedir);
+        int tamP = temp.CantidadProductosAPedir - 1;
+        imprimirProductos(&(temp.Productos), tamP);
+    }
+}
+
+void imprimirProductos(Producto **listado, int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        printf("entraaaaaaaaaaa");
+        Producto *temp = *(listado + i);
+        printf("\n Producto ID: %d", temp->ProductoID);
+        printf("\n      Producto '%s': ", temp->TipoProducto);
+        printf("\n              %d unidades pedidas", temp->Cantidad);
+        printf("\n              %d precio por unidad", temp->PrecioUnitario);
+        printf("\n              %d costo total del pedido", costoProducto(temp));
+    }
 }
